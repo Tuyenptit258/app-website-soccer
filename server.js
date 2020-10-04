0f133da9d4deef1008fb67e78142d5a6c3e75383
Tuyen <camgiacmanh1999@gmail.com>
2020-10-04 14:39:56 +0700

    update

--- server.js
+++ server.js
@@ -15,6 +15,25 @@ app.get('/', function(req,res){
   res.render('home.hbs')
 })
 
+
+
+const db = process.env.MONGODB_URL;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true
+    });
+    console.log("MongoDB is Connected...");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+
+
 app.listen(process.env.PORT, () => {
   console.log("start on")
 })
